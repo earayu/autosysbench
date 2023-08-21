d4336b93cec2f20b9e9a24f7cf2650b090701d1e
earayu <earayu@163.com>
2023-08-21 17:53:34 +0800

    add workload

--- autosysbench.sh
+++ autosysbench.sh
@@ -87,7 +87,7 @@ sysbench_vtgate_then_mysql_175_threads_loop() {
   done
 }
 
-sysbench_run() {
+sysbench_run_and_rest() {
   local podDefinition=$1
   create_and_wait_for_pod $podDefinition
   rest 60
@@ -103,27 +103,27 @@ main() {
 
   # sysbench_vtgate_then_mysql_175_threads_loop 5
 
-  sysbench_run sysbench_vtgate_4
-  sysbench_run sysbench_vtgate_8
-  sysbench_run sysbench_vtgate_16
-  sysbench_run sysbench_vtgate_25
-  sysbench_run sysbench_vtgate_50
-  sysbench_run sysbench_vtgate_75
-  sysbench_run sysbench_vtgate_100
-  sysbench_run sysbench_vtgate_125
-  sysbench_run sysbench_vtgate_150
-  sysbench_run sysbench_vtgate_175
+  sysbench_run_and_rest sysbench_vtgate_4
+  sysbench_run_and_rest sysbench_vtgate_8
+  sysbench_run_and_rest sysbench_vtgate_16
+  sysbench_run_and_rest sysbench_vtgate_25
+  sysbench_run_and_rest sysbench_vtgate_50
+  sysbench_run_and_rest sysbench_vtgate_75
+  sysbench_run_and_rest sysbench_vtgate_100
+  sysbench_run_and_rest sysbench_vtgate_125
+  sysbench_run_and_rest sysbench_vtgate_150
+  sysbench_run_and_rest sysbench_vtgate_175
 
-  sysbench_run sysbench_mysql_4
-  sysbench_run sysbench_mysql_8
-  sysbench_run sysbench_mysql_16
-  sysbench_run sysbench_mysql_25
-  sysbench_run sysbench_mysql_50
-  sysbench_run sysbench_mysql_75
-  sysbench_run sysbench_mysql_100
-  sysbench_run sysbench_mysql_125
-  sysbench_run sysbench_mysql_150
-  sysbench_run sysbench_mysql_175
+  sysbench_run_and_rest sysbench_mysql_4
+  sysbench_run_and_rest sysbench_mysql_8
+  sysbench_run_and_rest sysbench_mysql_16
+  sysbench_run_and_rest sysbench_mysql_25
+  sysbench_run_and_rest sysbench_mysql_50
+  sysbench_run_and_rest sysbench_mysql_75
+  sysbench_run_and_rest sysbench_mysql_100
+  sysbench_run_and_rest sysbench_mysql_125
+  sysbench_run_and_rest sysbench_mysql_150
+  sysbench_run_and_rest sysbench_mysql_175
 
 
   getPodLogs $test_result_path
