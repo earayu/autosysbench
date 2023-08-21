146d2a9905ac70405ed0204a88559e56d0dcd6a7
earayu <earayu@163.com>
2023-08-21 18:55:28 +0800

    refactor

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ cd autosysbench
 
 ```python
 
+cd plot
+
 pip3 install -r requirements.txt
 
 python3 main.py
--- run/normal.sh
+++ run/normal.sh
@@ -1,4 +1,9 @@
-source ../autosysbench.sh
+# source "$($(pwd)/../autosysbench.sh)"
+
+current_path=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+source "$current_path/../common/autosysbench.sh"
+
+
 
 main() {
   test_result_path="$(pwd)/data/sysbench1"
@@ -7,31 +12,31 @@ main() {
 
   # sysbench_vtgate_then_mysql_175_threads_loop 5
 
-  sysbench_run_and_rest sysbench_vtgate_4
-  sysbench_run_and_rest sysbench_vtgate_8
-  sysbench_run_and_rest sysbench_vtgate_16
-  sysbench_run_and_rest sysbench_vtgate_25
-  sysbench_run_and_rest sysbench_vtgate_50
-  sysbench_run_and_rest sysbench_vtgate_75
-  sysbench_run_and_rest sysbench_vtgate_100
-  sysbench_run_and_rest sysbench_vtgate_125
-  sysbench_run_and_rest sysbench_vtgate_150
-  sysbench_run_and_rest sysbench_vtgate_175
+  # sysbench_run_and_rest sysbench_vtgate_4
+  # sysbench_run_and_rest sysbench_vtgate_8
+  # sysbench_run_and_rest sysbench_vtgate_16
+  # sysbench_run_and_rest sysbench_vtgate_25
+  # sysbench_run_and_rest sysbench_vtgate_50
+  # sysbench_run_and_rest sysbench_vtgate_75
+  # sysbench_run_and_rest sysbench_vtgate_100
+  # sysbench_run_and_rest sysbench_vtgate_125
+  # sysbench_run_and_rest sysbench_vtgate_150
+  # sysbench_run_and_rest sysbench_vtgate_175
 
-  sysbench_run_and_rest sysbench_mysql_4
-  sysbench_run_and_rest sysbench_mysql_8
-  sysbench_run_and_rest sysbench_mysql_16
-  sysbench_run_and_rest sysbench_mysql_25
-  sysbench_run_and_rest sysbench_mysql_50
-  sysbench_run_and_rest sysbench_mysql_75
-  sysbench_run_and_rest sysbench_mysql_100
-  sysbench_run_and_rest sysbench_mysql_125
-  sysbench_run_and_rest sysbench_mysql_150
-  sysbench_run_and_rest sysbench_mysql_175
+  # sysbench_run_and_rest sysbench_mysql_4
+  # sysbench_run_and_rest sysbench_mysql_8
+  # sysbench_run_and_rest sysbench_mysql_16
+  # sysbench_run_and_rest sysbench_mysql_25
+  # sysbench_run_and_rest sysbench_mysql_50
+  # sysbench_run_and_rest sysbench_mysql_75
+  # sysbench_run_and_rest sysbench_mysql_100
+  # sysbench_run_and_rest sysbench_mysql_125
+  # sysbench_run_and_rest sysbench_mysql_150
+  # sysbench_run_and_rest sysbench_mysql_175
 
 
   getPodLogs $test_result_path
   transform /Users/earayu/Documents/GitHub/sysbench-output-parser/sysparser $test_result_path
 }
 
-main
+# main
