5d699c553bd636a7e177f7c2460296f19c1b400b
earayu <earayu@163.com>
2023-08-23 23:40:17 +0800

    fix workload

--- run/read_write_split_disable_with_monitor.py
+++ run/read_write_split_disable_with_monitor.py
@@ -1,10 +1,6 @@
 import sysbench
 
 
-def debug_workload():
-    return sysbench.generate_workload(times=100, read_pct=20, threads=[4, 8])
-
-
 def read_pct_20_workload():
     return sysbench.generate_workload(times=100, read_pct=20, threads=[4, 25, 50, 100, 200])
 
@@ -17,6 +13,6 @@ def read_pct_80_workload():
     return sysbench.generate_workload(times=100, read_pct=80, threads=[4, 25, 50, 100, 200])
 
 
-# sysbench.run_sysbench_tests('read_write_split_disable_with_read_pct_20', debug_workload(), enable_monitor=True)
-# sysbench.run_sysbench_tests('read_write_split_disable_with_read_pct_50', read_pct_50_workload(), enable_monitor=True)
+sysbench.run_sysbench_tests('read_write_split_disable_with_read_pct_20', read_pct_20_workload(), enable_monitor=True)
+sysbench.run_sysbench_tests('read_write_split_disable_with_read_pct_50', read_pct_50_workload(), enable_monitor=True)
 sysbench.run_sysbench_tests('read_write_split_disable_with_read_pct_80', read_pct_80_workload(), enable_monitor=True)
--- run/read_write_split_enable.py
+++ run/read_write_split_enable.py
@@ -13,6 +13,7 @@ def read_pct_80_workload():
     return sysbench.generate_workload(times=60, read_pct=80, threads=[4, 8, 16, 25, 50, 75, 100, 125, 150, 175, 200])
 
 
-sysbench.run_sysbench_tests('read_write_split_enable_with_read_pct_20', read_pct_20_workload(), enable_monitor=False)
-sysbench.run_sysbench_tests('read_write_split_enable_with_read_pct_50', read_pct_50_workload(), enable_monitor=False)
-sysbench.run_sysbench_tests('read_write_split_enable_with_read_pct_80', read_pct_80_workload(), enable_monitor=False)
+for i in range(1, 8):
+    sysbench.run_sysbench_tests('read_write_split_enable_with_read_pct_20', read_pct_20_workload(), enable_monitor=False)
+    sysbench.run_sysbench_tests('read_write_split_enable_with_read_pct_50', read_pct_50_workload(), enable_monitor=False)
+    sysbench.run_sysbench_tests('read_write_split_enable_with_read_pct_80', read_pct_80_workload(), enable_monitor=False)
